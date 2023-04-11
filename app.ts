49b3457db0347e60a6302f3a22c68ce2d13f71f5
gunhee-jeong <kyle4619@naver.com>
2023-04-11 23:38:24 +0900

    Add: 튜플 타입의 role 프로퍼티를 추가

--- app.ts
+++ app.ts
@@ -14,10 +14,16 @@ const printResult = true;
 
 add(number1, number2, printResult);
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
 
 let favoriteActivities: string[];
